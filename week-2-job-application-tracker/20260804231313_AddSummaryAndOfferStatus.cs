e5c2af09ee093596cb6ba7ec0b1f7c5622a2e1df
Denzeljnr <94689827+Denzeljnr@users.noreply.github.com>
2026-08-09 15:31:44 +0100

    Week 2: Job Application Tracker

---
+++ week-2-job-application-tracker/20260804231313_AddSummaryAndOfferStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSummaryAndOfferStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Notes",
+                table: "Applications",
+                newName: "Summary");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Summary",
+                table: "Applications",
+                newName: "Notes");
+        }
+    }
+}
