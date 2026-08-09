fb2c1e14a71bf1bff6374734267a24e8a154a187
Denzeljnr <94689827+Denzeljnr@users.noreply.github.com>
2026-08-09 15:40:03 +0100

    Week 2: Job Application Tracker

---
+++ week-2-job-application-tracker/week-2-job-tracker/blazor/JobTracker/Models/ProcessedEmail.cs
@@ -0,0 +1,8 @@
+﻿namespace JobTracker.Models;
+
+public class ProcessedEmail
+{
+    public int Id { get; set; }
+    public string GmailMessageId { get; set; } = "";
+    public DateTime ProcessedAt { get; set; } = DateTime.UtcNow;
+}
