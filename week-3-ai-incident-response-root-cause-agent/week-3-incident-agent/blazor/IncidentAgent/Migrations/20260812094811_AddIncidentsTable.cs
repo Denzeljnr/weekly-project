f0597e734564d8457183c9ac52032cc101fcb9c0
Denzeljnr <94689827+Denzeljnr@users.noreply.github.com>
2026-08-16 14:20:21 +0100

    Week 3: AI Incident Response & Root-Cause Agent

---
+++ week-3-ai-incident-response-root-cause-agent/week-3-incident-agent/blazor/IncidentAgent/Migrations/20260812094811_AddIncidentsTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IncidentAgent.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIncidentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "incidents",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    alert_id = table.Column<int>(type: "integer", nullable: false),
+                    summary = table.Column<string>(type: "text", nullable: false),
+                    likely_cause = table.Column<string>(type: "text", nullable: false),
+                    evidence = table.Column<string>(type: "text", nullable: false),
+                    affected_service = table.Column<string>(type: "text", nullable: false),
+                    confidence_score = table.Column<int>(type: "integer", nullable: false),
+                    recommended_action = table.Column<string>(type: "text", nullable: false),
+                    related_events_count = table.Column<int>(type: "integer", nullable: false),
+                    source = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_incidents", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "incidents");
+        }
+    }
+}
