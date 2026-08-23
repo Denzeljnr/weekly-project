90cb285d29bb8652456816b839855801039c8569
Denzeljnr <94689827+Denzeljnr@users.noreply.github.com>
2026-08-23 17:16:13 +0100

    Week 4: Semantic Search with Real RAG Engineering

---
+++ week-4-semantic-search-with-real-rag-engineering/week-4-rag-search/blazor/SemanticSearch/SemanticSearch/Interfaces/IIngestionService.cs
@@ -0,0 +1,8 @@
+﻿using SemanticSearch.Models;
+
+namespace SemanticSearch.Interfaces;
+
+public interface IIngestionService
+{
+    Task IngestAsync(string sourceDocument, string fullText, IProgress<IngestionProgress>? progress = null);
+}
