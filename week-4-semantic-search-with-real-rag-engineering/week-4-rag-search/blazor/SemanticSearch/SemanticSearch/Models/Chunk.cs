8513997be850e63fdf5bb85d95b1cfecd0637792
Denzeljnr <94689827+Denzeljnr@users.noreply.github.com>
2026-08-23 17:16:26 +0100

    Week 4: Semantic Search with Real RAG Engineering

---
+++ week-4-semantic-search-with-real-rag-engineering/week-4-rag-search/blazor/SemanticSearch/SemanticSearch/Models/Chunk.cs
@@ -0,0 +1,12 @@
+﻿using Pgvector;
+
+namespace SemanticSearch.Models;
+
+public class Chunk
+{
+    public int Id { get; set; }
+    public string SourceDocument { get; set; } = "";
+    public string Content { get; set; } = "";
+    public string ChunkingStrategy { get; set; } = "";
+    public Vector Embedding { get; set; } = null!;
+}
